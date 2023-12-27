6cc1c3ce1013ba830ce3c97858f51c0a4c41d263
Jay Chang <jaychang0917@gmail.com>
2023-12-27 21:54:15 +0800

    update maven publish script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,11 @@
 pluginManagement {
     val kotlinVersion: String by settings
     val kspVersion: String by settings
+    val publishVersion: String by settings
     plugins {
         id("com.google.devtools.ksp") version kspVersion
         kotlin("jvm") version kotlinVersion
+        id("com.vanniktech.maven.publish") version publishVersion
     }
     repositories {
         gradlePluginPortal()
